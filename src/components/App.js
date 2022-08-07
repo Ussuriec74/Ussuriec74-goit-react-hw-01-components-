ca5a9e9b408af4221a0cb422d7524db379c4c18d
Valeriy Semko <valsemko74@gmail.com>
2022-08-07 11:41:41 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import { Statistics } from 'components/statistics/Statistics';
 import data from '../data/data.json';
 import { FriendList } from 'components/frends/friendList/FriendList';
 import friends from '../data/friends.json';
+import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 import { Box } from 'components/Box';
 
 
@@ -14,6 +16,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Box>
   );
 };
